c924107e2840e4651691f7d87f336b9f8f6d7b3e
Kaysul Anik <60320090+kaysulislam@users.noreply.github.com>
2023-09-13 23:21:11 +0300

    Added initial skeleton for car catalogue section

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,8 @@
 import CustomButton from "./CustomButton"
+import CustomFilter from "./CustomFilter"
 import Footer from "./Footer"
 import Hero from "./Hero"
 import NavBar from "./NavBar"
+import SearchBar from "./SearchBar"
 
-export { Hero, CustomButton, NavBar, Footer }
+export { Hero, CustomButton, NavBar, Footer, SearchBar, CustomFilter }
