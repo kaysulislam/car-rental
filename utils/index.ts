1b1665c457ff65e2c2757c462fc5645b7fb5f829
Kaysul Anik <60320090+kaysulislam@users.noreply.github.com>
2023-09-17 19:36:21 +0300

    Fixed CarDetails styling; removed static/mocked content

--- utils/index.ts
+++ utils/index.ts
@@ -1,93 +1,20 @@
 require("dotenv").config()
 
-const cars = [
-    {
-        city_mpg: 23,
-        class: "compact car",
-        combination_mpg: 24,
-        cylinders: 4,
-        displacement: 1.6,
-        drive: "fwd",
-        fuel_type: "gas",
-        highway_mpg: 26,
-        make: "toyota",
-        model: "corolla",
-        transmission: "a",
-        year: 1993,
-    },
-    {
-        city_mpg: 23,
-        class: "compact car",
-        combination_mpg: 26,
-        cylinders: 4,
-        displacement: 1.6,
-        drive: "fwd",
-        fuel_type: "gas",
-        highway_mpg: 31,
-        make: "toyota",
-        model: "corolla",
-        transmission: "m",
-        year: 1993,
-    },
-    {
-        city_mpg: 23,
-        class: "compact car",
-        combination_mpg: 25,
-        cylinders: 4,
-        displacement: 1.8,
-        drive: "fwd",
-        fuel_type: "gas",
-        highway_mpg: 30,
-        make: "toyota",
-        model: "corolla",
-        transmission: "a",
-        year: 1993,
-    },
-    {
-        city_mpg: 23,
-        class: "compact car",
-        combination_mpg: 26,
-        cylinders: 4,
-        displacement: 1.8,
-        drive: "fwd",
-        fuel_type: "gas",
-        highway_mpg: 30,
-        make: "toyota",
-        model: "corolla",
-        transmission: "m",
-        year: 1993,
-    },
-    {
-        city_mpg: 23,
-        class: "small station wagon",
-        combination_mpg: 25,
-        cylinders: 4,
-        displacement: 1.8,
-        drive: "fwd",
-        fuel_type: "gas",
-        highway_mpg: 30,
-        make: "toyota",
-        model: "corolla wagon",
-        transmission: "a",
-        year: 1993,
-    },
-]
-
 export async function fetchCars() {
-    // const headers = {
-    //     "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
-    //     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-    // }
-    // const response = await fetch(
-    //     "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
-    //     {
-    //         headers: headers,
-    //     }
-    // )
+    const headers = {
+        "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
+        "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+    }
+    const response = await fetch(
+        "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+        {
+            headers: headers,
+        }
+    )
 
-    // const result = await response.json()
+    const result = await response.json()
 
-    return cars
+    return result
 }
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
