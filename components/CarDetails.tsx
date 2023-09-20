3ddc89fccf106236a08f69a2f51eee2251e7d395
Kaysul Anik <60320090+kaysulislam@users.noreply.github.com>
2023-09-20 23:01:19 +0300

    Added car images implementation from the api

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -5,6 +5,7 @@ import React from "react"
 import Image from "next/image"
 import { Fragment } from "react"
 import { Dialog, Transition } from "@headlessui/react"
+import { generateCarImageUrl } from "@utils"
 
 interface CarDetailsProps {
     isOpen: boolean
@@ -64,7 +65,10 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                                     <div className="flex-1 flex flex-col gap-3">
                                         <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
                                             <Image
-                                                src="/hero.png"
+                                                src={generateCarImageUrl(
+                                                    car,
+                                                    "angle"
+                                                )}
                                                 alt="car model"
                                                 fill
                                                 priority
@@ -74,7 +78,10 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                                         <div className="flex gap-3">
                                             <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                                                 <Image
-                                                    src="/hero.png"
+                                                    src={generateCarImageUrl(
+                                                        car,
+                                                        "29"
+                                                    )}
                                                     alt="car model"
                                                     fill
                                                     priority
@@ -83,7 +90,10 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                                             </div>
                                             <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                                                 <Image
-                                                    src="/hero.png"
+                                                    src={generateCarImageUrl(
+                                                        car,
+                                                        "33"
+                                                    )}
                                                     alt="car model"
                                                     fill
                                                     priority
@@ -92,7 +102,10 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                                             </div>
                                             <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                                                 <Image
-                                                    src="/hero.png"
+                                                    src={generateCarImageUrl(
+                                                        car,
+                                                        "13"
+                                                    )}
                                                     alt="car model"
                                                     fill
                                                     priority
